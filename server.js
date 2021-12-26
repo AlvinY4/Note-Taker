f04570d48e06fa8d5c77e12eb324689d99951818
Alvin Yearby <leroy.yearby@gmail.com>
2021-12-26 14:55:44 -0500

    updated server.js

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express'); 
+const fs = require('fs'); 
+const path = require('path'); 
+
+const PORT = process.env.PORT || 3000; 
+const app = express(); 
+
+app.use(express.static(__dirname));
+app.use(express.json()); 
+app.use(express.urlencoded({ extended: true})); 
+
+require('./routes/routes')(app); 
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT); 
+}); 
+
